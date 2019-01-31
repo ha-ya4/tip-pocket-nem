64b9f934987c0e9b01e60e07c00c780a38508037
haya <nemgrkz-rustzun8@yahoo.co.jp>
2019-01-31 16:49:17 +0900

    vuetifi追加と各ファイル作成

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue';
 import router from './router/router';
 import store from './vuex/store';
 import firebase from 'firebase';
+import Vuetify from 'vuetify'
 import './registerServiceWorker';
 
 import config from '@/firebase.ts';
+
+Vue.use(Vuetify, {})
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Transfer.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+
+})
+
+export default class Home extends Vue {}
+</script>
